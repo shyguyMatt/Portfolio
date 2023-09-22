af43d54e4a67aa3cbb77141fa0d3215392f89360
ShyguyMatt <mjherford@gmail.com>
2023-09-21 23:00:28 -0400

    new commit

---
+++ src/components/pages/About/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import img from './../../../assets/images/me.PNG'
+
+import './styles.css'
+
+export default function About() {
+
+  return(
+    <div id='aboutMe'>
+      <div id='bio'>
+        <h1>About ShyguyMatt</h1>
+        <p>I am a 23 year old Web Developer. Recently graduated from coding bootcamp. I am always eager to learn about new technologies. Currently my passion is Web Applications and PWAs. I am proficient in both the front and the back end of applications, but more specifically back end.</p>
+      </div>
+
+      <div id='gallery'>
+        <img src={img}></img>
+      </div>
+    </div>
+  )
+}
