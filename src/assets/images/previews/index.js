5dcac8933c48afcd64cabe2f4d7276a78926fce4
ShyguyMatt <mjherford@gmail.com>
2024-10-18 21:39:31 -0400

    delete everything and restart

--- src/assets/images/previews/index.js
+++
@@ -1,9 +0,0 @@
-import portfolio from './portfolioPreview.png'
-import maze from './maze.png'
-import readMe from './readMe.png'
-import weather from './weather.png'
-import connectMates from './connectMates.png'
-
-const images = [portfolio, maze, connectMates, readMe, weather]
-
-export { images }
